786f1752915cd6e8e85a0e2f42d7124dee176fb1
Firouz Karimov <fkarimov@gmail.com>
2020-04-13 09:57:02 -0600

    v2.2

--- Starter_Code/static/js/app.js
+++ Starter_Code/static/js/app.js
@@ -27,24 +27,14 @@ var trace = {
     type: "bar",
     text: otuLabels,
     orientation: 'h',
-    // width: 15,
+    
     };
 
 var data = [trace];
 var layout = {
     title: "Top 10 Bacteria Cultures Found",
     margin: { t: 30, l: 150 },
-    // xaxis: {title: "Sample Values",
-    //         range: [0,140]
-            
-    //             },
-    // yaxis: {
-    //         // range: [2700,3000],
-    //             },
-    // autosize: true,
-    // width: 700,
-    // height: 500,
-    // bargap: 0.5
+  
 };
 
 Plotly.newPlot("bar", data, layout);
@@ -58,10 +48,6 @@ d3.json("samples.json").then(function(data){
     var sampleValue = result.sample_values;
     var otuId = result.otu_ids;
     var otuLabels = result.otu_labels;
-
-    
-        // otuId.sort(function(a, b){return b - a})
-        // var otuId1 = otuId.slice(0,10)
 
     console.log(otuId);
 var trace1 = {
@@ -88,7 +74,7 @@ var layout = {
       Plotly.newPlot('plot', data, layout);
 });
 
-// function buildMetadata(sample) {
+
     d3.json("samples.json").then(function(data) {
       console.log(data);
       var metadata = data.metadata;
@@ -102,15 +88,13 @@ var layout = {
       // Use `.html("") to clear any existing metadata
       PANEL.html("");
       // Use `Object.entries` to add each key and value pair to the panel
-      // Hint: Inside the loop, you will need to use d3 to append new
-      // tags for each key-value in the metadata.
+    
       Object.entries(result).forEach(([key, value]) => {
         PANEL.append("h6").text(`${key.toUpperCase()}: ${value}`);
       });
-      // BONUS: Build the Gauge Chart
-    //   buildGauge(result.wfreq);
+    
     });
-  // }
+ 
 
 
 
